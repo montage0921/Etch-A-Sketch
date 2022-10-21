1196e813e1508d3b599480139120ed3b7768d3f4
montage0921 <garyshi0921@gmail.com>
2022-10-21 10:51:55 -0400

    Set Up A Hover Effect for Grid

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const canvas = document.querySelector(`.canvas`);
 
 /////////////////////////////////////////////////
 const gridPerLine = 16; //16x16
-const color = `white`; //grid color
+const color = `red`; //grid color
 
 let grids = ``;
-const grid = `<div class="grid"></div>`;
+const grid = `<div class="grid" ></div>`;
 
 for (let i = 1; i <= gridPerLine; i++) {
   for (let j = 1; j <= gridPerLine; j++) {
@@ -24,3 +24,12 @@ gridsHTML.forEach((grid) => (grid.style.backgroundColor = color));
 //   grid-template-rows: repeat(16, 1fr);
 canvas.style.gridTemplateColumns = `repeat(${gridPerLine}, 1fr)`;
 canvas.style.gridTemplateRows = `repeat(${gridPerLine}, 1fr)`;
+
+//Set up a "mouse hover" effect
+canvas.addEventListener(`mouseover`, function (e) {
+  e.target.style.backgroundColor = `black`;
+});
+
+canvas.addEventListener(`mouseout`, function (e) {
+  e.target.style.backgroundColor = color;
+});
