3f91c4ae88bd48e44b69e984e92acca3d384e790
montage0921 <garyshi0921@gmail.com>
2022-10-21 09:53:56 -0400

    Finish Grid of Square can change size and color accordingly

---
+++ main.js
@@ -0,0 +1,26 @@
+//////DOM Selectors///////
+const canvas = document.querySelector(`.canvas`);
+
+/////////////////////////////////////////////////
+const gridPerLine = 16; //16x16
+const color = `white`; //grid color
+
+let grids = ``;
+const grid = `<div class="grid"></div>`;
+
+for (let i = 1; i <= gridPerLine; i++) {
+  for (let j = 1; j <= gridPerLine; j++) {
+    grids += grid;
+  }
+}
+canvas.insertAdjacentHTML(`afterbegin`, grids);
+
+//assign color to CSS
+const gridsHTML = document.querySelectorAll(`.grid`);
+gridsHTML.forEach((grid) => (grid.style.backgroundColor = color));
+
+//generate grid of square using CSS Grid
+//  grid-template-columns: repeat(16, 1fr);
+//   grid-template-rows: repeat(16, 1fr);
+canvas.style.gridTemplateColumns = `repeat(${gridPerLine}, 1fr)`;
+canvas.style.gridTemplateRows = `repeat(${gridPerLine}, 1fr)`;
