2d6fc864143d8a488201be82622c066bbcda7fcb
montage0921 <garyshi0921@gmail.com>
2022-10-21 13:03:51 -0400

    Finish Slider Bar

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 //////DOM Selectors///////
 const canvas = document.querySelector(`.canvas`);
+const slider = document.querySelector(`.grid-size-slider`);
+const slideValue = document.querySelector(`.select-value`);
 
-/////////////////////////////////////////////////
-const gridPerLine = 16; //16x16
-const color = `white`; //grid color
-
+/////////////////////////////////////////////
+let gridPerLine; //16x16
 let grids = ``;
 const grid = `<div class="grid" ></div>`;
+const color = `red`;
 
-for (let i = 1; i <= gridPerLine; i++) {
-  for (let j = 1; j <= gridPerLine; j++) {
-    grids += grid;
+slider.addEventListener(`input`, function (e) {
+  let value = e.target.value;
+  slideValue.textContent = `${value}x${value}`;
+  gridPerLine = value;
+
+  grids = ``;
+  canvas.innerHTML = ``;
+
+  for (let i = 1; i <= gridPerLine; i++) {
+    for (let j = 1; j <= gridPerLine; j++) {
+      grids += grid;
+    }
   }
-}
-canvas.insertAdjacentHTML(`afterbegin`, grids);
+  canvas.insertAdjacentHTML(`afterbegin`, grids);
 
-//assign color to CSS
-const gridsHTML = document.querySelectorAll(`.grid`);
-gridsHTML.forEach((grid) => (grid.style.backgroundColor = color));
+  //assign color
+  const gridsHTML = document.querySelectorAll(`.grid`);
+  gridsHTML.forEach((grid) => (grid.style.backgroundColor = color));
 
-//generate grid of square using CSS Grid
-//  grid-template-columns: repeat(16, 1fr);
-//   grid-template-rows: repeat(16, 1fr);
-canvas.style.gridTemplateColumns = `repeat(${gridPerLine}, 1fr)`;
-canvas.style.gridTemplateRows = `repeat(${gridPerLine}, 1fr)`;
+  //generate grid of square using CSS Grid
+  canvas.style.gridTemplateColumns = `repeat(${gridPerLine}, 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${gridPerLine}, 1fr)`;
+});
 
 //Set up a "mouse hover" effect
 canvas.addEventListener(`mouseover`, function (e) {
